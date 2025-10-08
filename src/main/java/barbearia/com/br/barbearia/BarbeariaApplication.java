0b89ab96b7958c43f990d4a76f72c3c80d9ecbc7
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-10-08 11:36:40 -0300

    first commit

---
+++ src/main/java/barbearia/com/br/barbearia/BarbeariaApplication.java
@@ -0,0 +1,13 @@
+package barbearia.com.br.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/barbearia/com/br/barbearia/BarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package barbearia.com.br.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
